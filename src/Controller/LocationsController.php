828379da7231d15ce9d20a6137a8df3a386717d4
Milpas Franky <https://github.com/FMilpas/reservations2.git>
2022-04-14 20:55:46 +0200

    Controller Locations + templates + routes

---
+++ src/Controller/LocationsController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\Locations;
+use App\Repository\LocationsRepository;
+
+
+class LocationsController extends AbstractController
+{
+    #[Route('/locations', name: 'app_locations')]
+    public function index(LocationsRepository $repository): Response
+    {
+        //$repository = $this->getDoctrine()->getRepository(Locations::class);
+        $locations = $repository->findAll();
+
+        return $this->render('locations/index.html.twig', [
+            'locations' => $locations,
+            'resource' => 'lieux',
+        ]);
+    }
+
+    /**
+     * @Route("/locations/{id}", name="locations_show")
+     */
+    public function show($id)
+    {
+        $repository = $this->getDoctrine()->getRepository(Location::class);
+        $location = $repository->find($id);
+
+        return $this->render('locations/show.html.twig', [
+            'location' => $location,
+        ]);
+    }
+
+
+}
