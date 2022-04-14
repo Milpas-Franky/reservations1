049678aef106009f17435bb73ddba8d0a762bcb8
Milpas Franky <https://github.com/FMilpas/reservations2.git>
2022-04-14 21:08:09 +0200

    Controller Shows +templates + routes

---
+++ src/Controller/ShowsController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\Shows;
+use App\Repository\ShowsRepository;
+
+
+class ShowsController extends AbstractController
+{
+    #[Route('/shows', name: 'app_shows')]
+    public function index(ShowsRepository $repository): Response
+    {
+        //$repository = $this->getDoctrine()->getRepository(Shows::class);
+        $shows = $repository->findAll();
+        
+        return $this->render('shows/index.html.twig', [
+            'shows' => $shows,
+            'resource' => 'spectacles',
+        ]);
+    }
+
+        /**
+     * @Route("/shows/{id}", name="shows_show")
+     */
+    public function show($id)
+    {
+        $repository = $this->getDoctrine()->getRepository(Shows::class);
+        $show = $repository->find($id);
+
+        return $this->render('shows/show.html.twig', [
+            'show' => $show,
+        ]);
+    }
+
+}
