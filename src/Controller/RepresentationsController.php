19a14749e2f214e7f0c0753f1925887e35115623
Milpas Franky <https://github.com/FMilpas/reservations2.git>
2022-04-15 11:01:47 +0200

    Controller Representations + templates +routes

---
+++ src/Controller/RepresentationsController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\Representations;
+use App\Repository\RepresentationsRepository;
+
+
+class RepresentationsController extends AbstractController
+{
+    #[Route('/representations', name: 'app_representations')]
+    public function index(RepresentationsRepository $repository): Response
+    {
+        //$repository = $this->getDoctrine()->getRepository(Representations::class);
+        $representations = $repository->findAll();
+        
+        return $this->render('representations/index.html.twig', [
+            'representations' => $representations,
+            'resource' => 'représentations',
+        ]);
+    }
+
+    /**
+     * @Route("/representations/{id}", name="representations_show")
+     */
+    public function show($id)
+    {
+        $repository = $this->getDoctrine()->getRepository(Representations::class);
+        $representation = $repository->find($id);
+
+        return $this->render('representations/show.html.twig', [
+            'representation' => $representation,
+        ]);
+    }
+
+}
