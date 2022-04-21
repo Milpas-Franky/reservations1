0c657f64a63a906a850e4ac8c02da2bc979e2302
Milpas Franky <https://github.com/FMilpas/reservations2.git>
2022-04-21 13:44:18 +0200

    migrations, modif artistsTypes entity

--- src/Entity/Artists.php
+++ src/Entity/Artists.php
@@ -3,6 +3,9 @@
 namespace App\Entity;
 
 use App\Repository\ArtistsRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+
 use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity(repositoryClass: ArtistsRepository::class)]
@@ -19,7 +22,16 @@ class Artists
 
     #[ORM\Column(type: 'string', length: 60)]
     private $lastname;
+    
+     #[ORM\OneToMany(targetEntity : 'ArtistsTypes::class', mappedBy : 'artists', orphanRemoval :true)]
+    
+     private $types;
 
+     public function __construct()
+     {
+         $this->types = new ArrayCollection();
+     }
+ 
     public function getId(): ?int
     {
         return $this->id;
@@ -48,4 +60,35 @@ class Artists
 
         return $this;
     }
+
+    /**
+     * @return Collection|ArtistsTypes[]
+     */
+    public function getTypes(): Collection
+    {
+        return $this->types;
+    }
+
+    public function addType(ArtistsTypes $type): self
+    {
+        if (!$this->types->contains($type)) {
+            $this->types[] = $type;
+            $type->setArtist($this);
+        }
+
+        return $this;
+    }
+
+    public function removeType(ArtistsTypes $type): self
+    {
+        if ($this->types->removeElement($type)) {
+            // set the owning side to null (unless already changed)
+            if ($type->getArtist() === $this) {
+                $type->setArtist(null);
+            }
+        }
+
+        return $this;
+    }
+
 }
