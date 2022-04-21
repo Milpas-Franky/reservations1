f2d334c7ba6ef9e4f67f6c2d3a6aa73e8b051928
Milpas Franky <https://github.com/FMilpas/reservations2.git>
2022-04-21 10:42:05 +0200

    modif entités types & artists

--- src/Entity/Types.php
+++ src/Entity/Types.php
@@ -3,6 +3,9 @@
 namespace App\Entity;
 
 use App\Repository\TypesRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+
 use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity(repositoryClass: TypesRepository::class)]
@@ -18,6 +21,22 @@ class Types
     #[ORM\Column(type: 'string', length: 60)]
     private $type;
 
+    /**
+     * @ORM\ManyToMany(targetEntity="Artist::class", mappedBy="types")
+     */
+    private $artists;
+
+    public function __construct()
+    {
+        $this->artists = new ArrayCollection();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+
     public function getType(): ?string
     {
         return $this->type;
@@ -29,4 +48,32 @@ class Types
 
         return $this;
     }
+
+        /**
+     * @return Collection|Artists[]
+     */
+    public function getArtists(): Collection
+    {
+        return $this->artists;
+    }
+
+    public function addArtist(Artists $artist): self
+    {
+        if (!$this->artists->contains($artist)) {
+            $this->artists[] = $artist;
+            $artist->addType($this);
+        }
+
+        return $this;
+    }
+
+    public function removeArtist(Artists $artist): self
+    {
+        if ($this->artists->removeElement($artist)) {
+            $artist->removeType($this);
+        }
+
+        return $this;
+    }
+
 }
