55b6aba4a02e65f9748db90281c17089c93f86d8
Milpas Franky <https://github.com/FMilpas/reservations2.git>
2022-05-06 10:26:57 +0200

    modif ShowsController

--- src/Controller/ShowsController.php
+++ src/Controller/ShowsController.php
@@ -27,11 +27,8 @@ class ShowsController extends AbstractController
         /**
      * @Route("/shows/{id}", name="shows_show")
      */
-<<<<<<< HEAD
     public function show(ManagerRegistry $doctrine,$id)
-=======
-    public function show(ManagerRegistry $doctrine, $id)
->>>>>>> tablePivot
+
     {
         $repository = $doctrine->getRepository(Shows::class);
         $show = $repository->find($id);
